0a950f47c973d8ae411bcaf93dc907753414836c
James Le Cuirot <james.le-cuirot@yakara.com>
2012-06-15 11:32:02 +0100

    [FEATURE] Add terminate feature

--- lib/future-resource.rb
+++ lib/future-resource.rb
@@ -26,6 +26,7 @@ class FutureResource
   def initialize
     @resource_lock          = Monitor.new
     @resource_value_blocker = @resource_lock.new_cond
+    @terminated             = false
   end
   
   ##
@@ -38,19 +39,32 @@ class FutureResource
   end
 
   ##
-  # Returns the value of a specific resource, optionally waiting for `timeout` seconds before raising a Timeout::Error exception.
-  # When called on a not set resource without a timeout, raises a deadlock.
+  # Checks if the attempt to read the resource has been terminated.
+  #
+  # @return [Boolean]
+  #
+  def terminated?
+    !!@resource_lock.synchronize { @terminated }
+  end
+
+  ##
+  # Returns the value of a specific resource, optionally waiting for `timeout` seconds before
+  # raising a Timeout::Error exception, or raising a FutureResource::Terminated exception if
+  # the attempt to read the resource is terminated early by another thread.  When called on
+  # an unset resource without a timeout, raises a deadlock.
   #
   # @param [Integer] timeout number of seconds to wait for the resource to become ready
   #
   # @raise [Timeout::Error] if timeout expires and resource is not ready
+  # @raise [FutureResource::Terminated] if the attempt to read the resource is terminated by another thread
   #
   # @return [Object]
   #
   def resource(timeout = nil)
     Timeout::timeout timeout do
       @resource_lock.synchronize do
-        @resource_value_blocker.wait unless defined? @resource
+        @resource_value_blocker.wait unless set_yet? or terminated?
+        raise Terminated if terminated?
         @resource
       end
     end
@@ -58,17 +72,29 @@ class FutureResource
   
   ##
   # Sets the value for the resource, making it available for all waiting and following reads.
-  # Resource values can only be set once.
+  # Resource values can only be set once.  Calling this method on a terminated resource is
+  # ineffective.
   #
   # @param [Object] resource any value to be set for the resource
   #
   # @raise [FutureResource::ResourceAlreadySet] if resource is already set
+  #
   def resource=(resource)
-    @resource_lock.synchronize do
-      raise ResourceAlreadySetException if defined? @resource
+    set_or_terminate do
       @resource = resource
-      @resource_value_blocker.broadcast
-      @resource_value_blocker = nil # Don't really need it anymore.
+    end
+  end
+
+  ##
+  # Terminates the attempt to read the resource early, causing those waiting and any
+  # following reads to raise a FutureResource::Terminated exception. Subsequently calling
+  # this method again is ineffective.
+  #
+  # @raise [FutureResource::ResourceAlreadySet] if resource is already set
+  #
+  def terminate
+    set_or_terminate do
+      @terminated = true
     end
   end
 
@@ -78,6 +104,27 @@ class FutureResource
   class ResourceAlreadySetException < StandardError
     def initialize
       super "Cannot set this resource twice!"
+    end
+  end
+
+  ##
+  # Raised when the attempt to read the resource is terminated early.
+  #
+  class Terminated < StandardError
+    def initialize
+      super "Resource read attempt terminated"
+    end
+  end
+
+  private
+
+  def set_or_terminate
+    @resource_lock.synchronize do
+      return if terminated?
+      raise ResourceAlreadySetException if set_yet?
+      yield
+      @resource_value_blocker.broadcast
+      @resource_value_blocker = nil # Don't really need it anymore.
     end
   end
 end
